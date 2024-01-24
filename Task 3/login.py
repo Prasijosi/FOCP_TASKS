4453713b58106255485376e560f44f1895d3c8ab
Prasijosi <jprashiddha5@gmail.com>
2024-01-24 13:10:11 +0545

    CRUD Password

--- Task 3/login.py
+++ Task 3/login.py
@@ -5,15 +5,12 @@ def login(username, password):
     # Check if the username exists before prompting for a password
     info_user = main.retrieve_user_data(username)
     if not info_user:
-        print("Username not found. Please try again.\n")
+        print("Error:Invalid user id or password.")
         return False
 
     # If username exists, proceed with password verification
     if main.password_encryption(password) == info_user[1]:
         return True
-
-    print("Incorrect password. Please try again.\n")
-    return False
 
 username = input("Enter username:").lower()
 password = input("Enter a password:")
@@ -21,4 +18,4 @@ password = input("Enter a password:")
 if login(username, password):
         print("Welcome!!")
 else:
-        print("Incorrect Username or Password")
+        print("Incorrect password\n")
