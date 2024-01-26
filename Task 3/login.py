ea1dbb5fc4917bdabb0f07f66fd0d176183a5738
Prasijosi <110450688+Prasijosi@users.noreply.github.com>
2024-01-26 11:37:41 +0545

    CRUD Password

--- Task 3/login.py
+++ Task 3/login.py
@@ -1,4 +1,5 @@
 import main
+import getpass
 
 def login(username, password):
     # Check if the username exists before prompting for a password
@@ -12,7 +13,7 @@ def login(username, password):
         return True
 
 username = input("Enter username:").lower()
-password = input("Enter a password:")
+password = getpass.getpass("Enter a password:")
 
 if login(username, password):
         print("Welcome!!")
